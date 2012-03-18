286c5b6b3d613b6dba236d903a99c55ede231018
Paul M. Gurniak <pgurniak@seas.upenn.edu>
2012-03-18 14:20:12 -0400

    Finished testers for the classes with "

--- CalorieCounter/src/edu/upenn/cis350/FoodGenerator.java
+++ CalorieCounter/src/edu/upenn/cis350/FoodGenerator.java
@@ -30,7 +30,7 @@ public class FoodGenerator {
 				300, 400, 50);
 		FoodItem apple = new FoodItem(res.getDrawable(R.drawable.redapple),
 				res.getString(R.string.redapple),
-				80, 120, 40);
+				50, 90, 10);
 		unseenItems.add(fries1);
 		unseenItems.add(fries2);
 		unseenItems.add(apple);
