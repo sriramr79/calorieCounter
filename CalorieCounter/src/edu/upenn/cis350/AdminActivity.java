28fd2cdf13f95c166cbb44b1f9f03152e8ae5545
Sriram Radhakrishnan <sriramr79@gmail.com>
2012-04-23 13:07:20 -0400

    added detailed admin page listing statistics (username and score only for now)

--- CalorieCounter/src/edu/upenn/cis350/AdminActivity.java
+++ CalorieCounter/src/edu/upenn/cis350/AdminActivity.java
@@ -3,14 +3,17 @@ package edu.upenn.cis350;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class AdminActivity extends Activity {
+public class AdminActivity extends ListActivity {
 
 
+	ArrayList<String> names;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -18,11 +21,13 @@ public class AdminActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.admin);
 		
-		ListView list = (ListView) findViewById(R.id.adminList);
-		HashMap<String, String> names = IOBasic.allNames();
+		names = new ArrayList<String>();
+		
+		HashMap<String, String> allNames = IOBasic.allNames();
 		ArrayList<String> values = new ArrayList<String>();
-		for (String usn : names.keySet()) {
-			values.add(names.get(usn) + " (" + usn + ")");
+		for (String usn : allNames.keySet()) {
+			values.add(allNames.get(usn) + " (" + usn + ")");
+			names.add(usn);
 		}
 		
 		String[] items = new String[values.size()];
@@ -30,9 +35,21 @@ public class AdminActivity extends Activity {
 		
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, items);
 		
-		list.setAdapter(adapter);
-
+		setListAdapter(adapter);
 	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		String usn = names.get(position);
+		Intent i = new Intent(this, AdminDetailActivity.class);
+		i.putExtra(Constants.UNEXTRA, usn);
+		startActivity(i);
+	}
+	
+	
+	
+	
 	
 	
 
